1cc0fd1299ece7816a17e36abfc8ba6eb992d7cc
cyypherus <ejjonny@gmail.com>
2025-09-10 20:16:08 -0600

    remove old code, onwards

--- src/traits/drawable.rs
+++
@@ -1,9 +0,0 @@
-use crate::models::Area;
-
-/// An object which can be drawn
-///
-/// See `nodes::draw_object`
-pub trait Drawable<T, U> {
-    /// Called with the laid-out position for this node
-    fn draw(&mut self, area: Area, t: &mut T, u: &mut U, visible: bool);
-}
--- src/traits/mod.rs
+++
@@ -1,5 +0,0 @@
-mod drawable;
-mod nodetrait;
-
-pub use drawable::Drawable;
-pub(crate) use nodetrait::NodeTrait;
