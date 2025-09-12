f55c5c933da64ac2d54b25500401f7f83d08cf69
cyypherus <ejjonny@gmail.com>
2025-09-12 17:38:28 -0600

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -15,44 +15,17 @@ See [`Layout`] for setup.
 See [`Node`] for layout customization.
 */
 
-// mod constraints;
-// mod debug;
-// mod drawable;
-// mod layout;
-// pub use layout::Layout;
-// mod modifiers;
-// mod node;
-// pub use node::Node;
-// mod node_cache;
-// mod scoper;
-// mod subtree;
-// mod tests;
+mod api;
+mod passes;
+mod tests;
+mod tree;
+mod types;
 
-pub mod functional_layout;
+pub use types::{Align, Area, Layout};
 
-// mod mvp;
-
-// pub use mvp::{Layout, Node};
-
-// pub mod models {
-//     pub use crate::mvp::{Align, Area, Size};
-// }
-// pub mod nodes {
-//     pub use crate::mvp::{
-//         area_reader, column, column_aligned, column_spaced, column_spaced_aligned, draw, dynamic,
-//         empty, intermediate, row, row_aligned, row_spaced, row_spaced_aligned, space, stack,
-//         stack_aligned,
-//     };
-// }
-// pub mod node {
-//     pub use crate::mvp::Node;
-// }
-
-// /// Traits for layout definitions
-// pub mod traits;
-
-// /// Structs involved in layout definitions
-// pub mod models;
-
-// /// Layout core node construction
-// pub mod nodes;
+pub mod nodes {
+    pub use crate::api::{
+        area_reader, column, column_aligned, column_spaced, column_spaced_aligned, draw, empty,
+        row, row_aligned, row_spaced, row_spaced_aligned, space, stack, stack_aligned,
+    };
+}
